20d307ceb2e450791c4286d3899d70c89ef0062a
Trey <treycoopermusic@gmail.com>
2017-09-17 17:44:41 -0400

    install react-redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
-
+import { Provider } from 'react-redux'
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -14,6 +14,8 @@ const store = createStore(
 
 
 ReactDOM.render(
-  <App store={ store } />,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
