e5490add8e95d9e8bf4035b3ad48a3940a90f5c1
Trey <treycoopermusic@gmail.com>
2017-09-18 11:56:23 -0400

    add food reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
 
   render() {
+          console.log('Props', this.props)
     return (
       <div>
         Hello World
@@ -10,4 +13,29 @@ class App extends Component {
     )
   }
 }
-export default App
+
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
+'friday'];
+
+return{
+    calendar: dayOrder.map((day) => ({
+      day,
+      meal: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    }))
+  }
+}
+
+function mapDispatchToProps (dispatch){
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
